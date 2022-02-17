5ed3901e93de5a9806f560820ac11cf28745daa7
theo <theo.kossoy@gmail.com>
2022-02-17 20:55:09 +0300

    feat: add polygon

--- lab_01/main.cpp
+++ lab_01/main.cpp
@@ -1,3 +1,5 @@
+#include <stdexcept>
+
 class Point {
     private:
         int x_;
@@ -9,6 +11,8 @@ class Point {
             ,y_(y_val)
         {}
 
+        ~Point();
+
         Point(const Point& p)
             :x_(p.x_)
             ,y_(p.y_)
@@ -18,6 +22,13 @@ class Point {
             x_ = p.x_;
             y_ = p.y_;
             return *this;
+        }
+
+        bool operator==(const Point& p) {
+            if ((x_ == p.x_) && (y_ == p.y_)) {
+                return true;
+            };
+            return false;
         }
         
         int X() const {
@@ -32,24 +43,103 @@ class Point {
 class BrokenLine {
     private:
         Point* coords_;
+        int points_;
 
     public:
-        explicit BrokenLine(Point* coords)
+        explicit BrokenLine(Point* coords, int points)
             :coords_(coords)
-        {}
+            ,points_(points)
+        {
+            //validate
+        }
+
+        ~BrokenLine();
 
         BrokenLine(const BrokenLine& bl)
             :coords_(bl.coords_)
+            ,points_(bl.points_)
         {}
 
         BrokenLine operator=(const BrokenLine& bl) {
             coords_ = bl.coords_;
+            points_ = bl.points_;
             return *this;
         }
 
         Point* Coords() {
             return coords_;
         }
+
+        int Points() {
+            return points_;
+        }
+
+        int Length() {
+            //for point in points
+        }
+};
+
+class ClosedBrokenLine: public BrokenLine {
+    public:
+        explicit ClosedBrokenLine(Point* coords, int points)
+            :BrokenLine(coords, points)
+        {
+            if (!(coords[0] == coords[points - 1])) {
+                throw std::invalid_argument("The line is not closed!");
+            };
+        }
+};
+
+class Polygon {
+    private:
+        Point* coords_;
+        int points_;
+
+    public:
+        explicit Polygon(Point* coords, int points)
+            :coords_(coords)
+            ,points_(points)
+        {
+            // validate
+        }
+
+        ~Polygon();
+
+        Polygon(const Polygon& p)
+            :coords_(p.coords_)
+            ,points_(p.points_)
+        {}
+
+        Polygon operator=(const Polygon& p) {
+            coords_ = p.coords_;
+            points_ = p.points_;
+            return *this;
+        }
+
+        Point* Coords() {
+            return coords_;
+        }
+
+        int Points() {
+            return points_;
+        }
+
+        int Perimeter() {
+            
+        }
+
+        float Area() {
+
+        }
+};
+
+class Triangle: public Polygon {
+    public:
+        explicit Triangle(Point* coords)
+            :Polygon(coords, 3)
+        {}
+    
+    
 };
 
 int main() {
